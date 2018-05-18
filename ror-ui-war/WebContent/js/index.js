0ff071db47f7bce0ef296e912a4e40a29775fddd
ror20 <38526358+ror20@users.noreply.github.com>
2018-05-18 14:52:57 +0530

    Changed check user url to point to api gateway

--- ror-ui-war/WebContent/js/index.js
+++ ror-ui-war/WebContent/js/index.js
@@ -1,19 +1,20 @@
 function openNav() {
-	var x = document.getElementById("navDemo");
-	if (x.className.indexOf("w3-show") == -1) {
-		x.className += " w3-show";
-	} else {
-		x.className = x.className.replace(" w3-show", "");
-	}
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
 }
 
 var request;
 function checkUserIdIFExist() {
 	var v = document.signupform.rorUserId.value;
-	var url = "https://ror-rest.herokuapp.com/rest/checkUserExist/" + v;
-	if (!v) {
-		return;
-	}
+		var url = "https://ror20-api.herokuapp.com/rorAPIGateway/checkUserExist/"
+				+ v;
+		if(!v){
+			return;
+		}
 	if (window.XMLHttpRequest) {
 		request = new XMLHttpRequest();
 	} else if (window.ActiveXObject) {
@@ -22,6 +23,7 @@ function checkUserIdIFExist() {
 
 	try {
 		request.onreadystatechange = getInfo;
+		req.setRequestHeader('Authorization', 'Basic YWthc2g6YWthc2g=' );
 		request.open("GET", url, true);
 		request.send();
 	} catch (e) {
@@ -32,30 +34,33 @@ function checkUserIdIFExist() {
 function getInfo() {
 	if (request.readyState == 4) {
 		var val = request.responseText;
-		if (val == 'true') {
+		if(val == 'true'){
 			document.getElementById('status').innerHTML = 'Not available';
 			$('#status').css('color', 'red');
-		} else {
+		}
+		else{
 			document.getElementById('status').innerHTML = 'available';
 			$('#status').css('color', 'green');
 		}
 	}
 }
-$(document).ready(function() {
-	$('#rorUserPassword, #repassword').on('keyup', function() {
-		if (!$('#repassword').val()) {
-			return;
-		}
-		if ($('#rorUserPassword').val() == $('#repassword').val()) {
-			$('#message').html('Matching').css('color', 'green');
-			$('#submitDiv').show();
-		} else {
-			$('#message').html('Not Matching').css('color', 'red');
-			$('#submitDiv').hide();
-		}
-	});
+$(document).ready(function(){
+ $('#rorUserPassword, #repassword').on('keyup', function() {
+	if(!$('#repassword').val()){
+		return;
+	}
+	if ($('#rorUserPassword').val() == $('#repassword').val()) {
+		$('#message').html('Matching').css('color', 'green');
+		$('#submitDiv').show();
+	} else{
+		$('#message').html('Not Matching').css('color', 'red');
+	$('#submitDiv').hide();
+	}
+});
+ 
 
-	$('#forgotPasswordLink').click(function() {
-		$('#forgotPasswordDiv').show();
-	});
+
+$('#forgotPasswordLink').click(function(){
+	$('#forgotPasswordDiv').show();
+});
 });
